a7d6b95448d28c73acbd011963257cc3e1867ea7
GRapple <grpel2108@gmail.com>
2021-12-01 11:46:03 +0900

    챗봇 insert시 NPE 방지
    displayElement가 null경우 에러 발생

--- api-server/src/main/java/kr/co/eicn/ippbx/server/service/WebchatBotDisplayElementService.java
+++ api-server/src/main/java/kr/co/eicn/ippbx/server/service/WebchatBotDisplayElementService.java
@@ -6,32 +6,33 @@ import kr.co.eicn.ippbx.model.form.WebchatBotDisplayElementFormRequest;
 import kr.co.eicn.ippbx.model.form.WebchatBotFormRequest;
 import kr.co.eicn.ippbx.server.repository.eicn.WebchatBotDisplayElementRepository;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @AllArgsConstructor
 @Service
 public class WebchatBotDisplayElementService extends ApiBaseService {
-    private final Logger logger = LoggerFactory.getLogger(WebchatBotDisplayElementService.class);
-
     private final WebchatBotDisplayElementRepository webchatBotDisplayElementRepository;
 
     public void insertDisplayElement(Integer displayId, WebchatBotFormRequest.DisplayElement elementInfo) {
-        WebchatBotDisplayElementFormRequest data = new WebchatBotDisplayElementFormRequest();
+        if (elementInfo != null) {
+            WebchatBotDisplayElementFormRequest data = new WebchatBotDisplayElementFormRequest();
 
-        data.setDisplayId(displayId);
-        data.setOrder(elementInfo.getOrder());
-        data.setTitle(elementInfo.getTitle());
-        data.setContent(elementInfo.getContent());
-        data.setImage(elementInfo.getImage());
-        data.setUrl(elementInfo.getUrl());
+            data.setDisplayId(displayId);
+            data.setOrder(elementInfo.getOrder());
+            data.setTitle(elementInfo.getTitle());
+            data.setContent(elementInfo.getContent());
+            data.setImage(elementInfo.getImage());
+            data.setUrl(elementInfo.getUrl());
 
-        webchatBotDisplayElementRepository.insert(data);
+            webchatBotDisplayElementRepository.insert(data);
+        } else
+            log.info("Display" + displayId + "element is null");
     }
 
     public void deleteByDisplayIdList(List<Integer> displayIdList) {
