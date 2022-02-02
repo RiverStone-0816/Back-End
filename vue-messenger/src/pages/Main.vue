6e8d18983414acf71532507c8603fc1239e17d0e
tinywind <tinywind0@gmail.com>
2022-02-03 04:02:35 +0900

    fix: 고객메신저, is_custinput_enable 를 참조해서 고객의 인풋을 enable/disable 시킨다

--- vue-messenger/src/pages/Main.vue
+++ vue-messenger/src/pages/Main.vue
@@ -487,9 +487,9 @@ export default {
         })
         .on('webchatsvc_message', data => {
           this.lastReceiveMessageType = data.message_type
+          this.inputEnable = data?.message_data?.is_custinput_enable === 'Y'
 
           if (data.message_type === 'intro') {
-            this.inputEnable = data.message_data.is_chatt_enable === 'Y'
             this.backgroundColor = data.message_data.bgcolor
             this.displayName = data.message_data.display_company_name
 
