e73241bc89578edd4318df95146b44ba95b6f1dc
박형석 <phsk2000@gmail.com>
2022-11-29 09:35:44 +0900

    이미지 업로드 오류 수정  - CHATBOT 폴더 만들기 추가.

--- api-server/src/main/java/kr/co/eicn/ippbx/server/service/WebChattingConfigService.java
+++ api-server/src/main/java/kr/co/eicn/ippbx/server/service/WebChattingConfigService.java
@@ -14,6 +14,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -106,6 +107,9 @@ public class WebChattingConfigService extends ApiBaseService {
 
     public String uploadImage(MultipartFile image) {
         final Path newPath = Paths.get(replace(savePath, "{0}", g.getUser().getCompanyId()));
+        final File file = new File(replace(savePath, "{0}", g.getUser().getCompanyId()));
+        if(!file.exists())
+            file.mkdir();
 
         return imageFileStorageService.uploadImage(newPath, image);
     }
