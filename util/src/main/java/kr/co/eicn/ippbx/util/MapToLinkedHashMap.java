9a2b433fc6472a5331bf116ea770c970f73cedf5
박형석 <phsk2000@gmail.com>
2023-03-16 16:55:50 +0900

    Map 정렬 함수 배포.

---
+++ util/src/main/java/kr/co/eicn/ippbx/util/MapToLinkedHashMap.java
@@ -0,0 +1,19 @@
+package kr.co.eicn.ippbx.util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MapToLinkedHashMap {
+    public <KEY extends Comparable<? super KEY>, VALUE> LinkedHashMap<KEY, VALUE> toLinkedHashMapByKey(Map<KEY, VALUE> map) {
+        return map.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o, n) -> o, LinkedHashMap::new));
+    }
+
+    public <KEY, VALUE extends Comparable<? super VALUE>> LinkedHashMap<KEY, VALUE> toLinkedHashMapByValue(Map<KEY, VALUE> map) {
+        return map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o, n) -> o, LinkedHashMap::new));
+    }
+}
