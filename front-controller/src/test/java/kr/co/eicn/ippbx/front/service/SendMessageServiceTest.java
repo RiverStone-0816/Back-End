8aaebcaa81423d2c84c6e424aa81751619dff9db
박형석 <phsk2000@gmail.com>
2023-03-21 17:49:25 +0900

    test

--- front-controller/src/test/java/kr/co/eicn/ippbx/front/service/SendMessageServiceTest.java
+++ front-controller/src/test/java/kr/co/eicn/ippbx/front/service/SendMessageServiceTest.java
@@ -2,7 +2,6 @@ package kr.co.eicn.ippbx.front.service;
 
 import kr.co.eicn.ippbx.front.service.api.application.sms.MessageSendService;
 import kr.co.eicn.ippbx.model.form.SendMessageFormRequest;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
@@ -10,14 +9,16 @@ import java.util.Collections;
 
 public class SendMessageServiceTest {
 
+/*
     final MessageSendService service = new MessageSendService();
 
     @SneakyThrows
-    //@Test
+    @Test
     public void test(){
         final SendMessageFormRequest request = new SendMessageFormRequest();
         request.setContent("MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송MMS테스트전송");
         request.setTargetNumbers(Collections.singletonList("01097160512"));
         service.post(request);
     }
+*/
 }
