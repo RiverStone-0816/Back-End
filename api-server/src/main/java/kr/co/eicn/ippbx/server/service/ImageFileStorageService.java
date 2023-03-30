396a2cb71777073c0c19f8b2ac3201fe5d6b7902
박형석 <phsk2000@gmail.com>
2023-03-30 09:13:33 +0900

    import 삭제

--- api-server/src/main/java/kr/co/eicn/ippbx/server/service/ImageFileStorageService.java
+++ api-server/src/main/java/kr/co/eicn/ippbx/server/service/ImageFileStorageService.java
@@ -7,7 +7,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
