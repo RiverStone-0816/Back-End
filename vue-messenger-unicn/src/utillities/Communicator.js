04375466a9517aa29c0810fc72d14cef03932a9c
박형석 <phsk2000@gmail.com>
2024-01-18 17:02:46 +0900

    유니씨엔쳇봇 수정

--- vue-messenger-unicn/src/utillities/Communicator.js
+++ vue-messenger-unicn/src/utillities/Communicator.js
@@ -159,5 +159,12 @@ Communicator.prototype.sendWebrtcReady = function (type, data) {
         message_data: data,
     }))
 }
+Communicator.prototype.sendFile = function (fileName,file) {
+    this.socket.emit('webchatcli_upload', Object.assign(this.request,{
+        message_id: this.getMessageId(),
+        message_type: 'custom_file',
+        message_data: {last_receive_message_type: 'upload_accept', file_name: fileName, file_data: file},
+    }))
+}
 
 export default Communicator
