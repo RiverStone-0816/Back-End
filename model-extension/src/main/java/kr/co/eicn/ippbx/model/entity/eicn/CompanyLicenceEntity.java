31d3f772d56bde84387c0bac3d1e333b217f1a9d
이정원 <sunny@eicn.co.kr>
2024-11-14 11:03:28 +0900

    refactor: 라이선스 주석 수정

--- model-extension/src/main/java/kr/co/eicn/ippbx/model/entity/eicn/CompanyLicenceEntity.java
+++ model-extension/src/main/java/kr/co/eicn/ippbx/model/entity/eicn/CompanyLicenceEntity.java
@@ -7,16 +7,16 @@ import lombok.ToString;
 @ToString
 @Data
 public class CompanyLicenceEntity {
-	private LicenseInfo recordLicense;  // 레코드 라이센스 정보
-	private LicenseInfo pdsLicense;     // PDS 라이센스 정보
-	private LicenseInfo statLicence;    // 통계,모니터링
-	private LicenseInfo ctiLicence;    // 상담원연결여부 정보
-	private LicenseInfo talkLicense;    // 상담톡 라이센스 정보
-	private LicenseInfo emailLicense;   // 이메일상담여부 라이센스 정보
-	private LicenseInfo chattLicense;	// 채팅 라이센스 정보
-	private LicenseInfo sttLicense;		// stt 라이센스 정보
-	private LicenseInfo softPhoneLicense; // 소프트폰 라이선스 정보
-	private LicenseInfo astinLicense;	// astin 라이선스 정보
-	private LicenseInfo astoutLicense;	// astout 라이선스 정보
-	private LicenseInfo aststtLicense;	// aststt 라이선스 정보
+    private LicenseInfo recordLicense;		// 녹취 라이센스 정보
+    private LicenseInfo pdsLicense;			// PDS 라이센스 정보
+    private LicenseInfo statLicence;		// 통계/모니터링 라이센스 정보
+    private LicenseInfo ctiLicence;			// 상담원 연결여부 라이센스 정보
+    private LicenseInfo talkLicense;		// 상담톡 라이센스 정보
+    private LicenseInfo emailLicense;		// 이메일 라이센스 정보
+    private LicenseInfo chattLicense;		// 메신저(채팅) 라이센스 정보
+    private LicenseInfo sttLicense;			// TA(STT) 라이센스 정보
+    private LicenseInfo softPhoneLicense;	// 소프트폰 라이선스 정보
+    private LicenseInfo astinLicense;		// astin 라이선스 정보
+    private LicenseInfo astoutLicense;		// astout 라이선스 정보
+    private LicenseInfo aststtLicense;		// aststt 라이선스 정보
 }
