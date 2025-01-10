cb1921a737f2842913ca82acde3b5ef221dd1eab
RiverStone-0816 <kangrack333@gmail.com>
2025-01-10 11:08:00 +0900

    refactor : 상담 API 구현  - 상담사에대한 상담내용 DB 저장  - SEQ 넘버 생성 로직 추가

--- api-server/src/main/java/kr/co/eicn/ippbx/server/controller/api/provision/result/ProvisionResultApiController.java
+++ api-server/src/main/java/kr/co/eicn/ippbx/server/controller/api/provision/result/ProvisionResultApiController.java
@@ -1,11 +1,15 @@
 package kr.co.eicn.ippbx.server.controller.api.provision.result;
 
+import static kr.co.eicn.ippbx.util.JsonResult.data;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.validation.Valid;
 import kr.co.eicn.ippbx.model.form.provision.ProvisionResultCustomInfoFormRequest;
 import kr.co.eicn.ippbx.server.controller.api.ApiBaseController;
+import kr.co.eicn.ippbx.server.repository.customdb.ProvisionResultRepository;
 import kr.co.eicn.ippbx.server.repository.eicn.MaindbGroupRepository;
 import kr.co.eicn.ippbx.server.service.MaindbCustomInfoService;
 import kr.co.eicn.ippbx.server.service.ResultCustomInfoService;
@@ -19,10 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
-import static kr.co.eicn.ippbx.util.JsonResult.data;
-
 /**
  * [API 제공용] 상담 API
  */
@@ -33,19 +33,17 @@ import static kr.co.eicn.ippbx.util.JsonResult.data;
 @RequestMapping(value = "api/provision/result", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProvisionResultApiController extends ApiBaseController {
 
-    private final MaindbGroupRepository   maindbGroupRepository;
-    private final MaindbCustomInfoService maindbCustomInfoService;
-    private final ResultCustomInfoService resultCustomInfoService;
+  private final MaindbGroupRepository maindbGroupRepository;
+  private final MaindbCustomInfoService maindbCustomInfoService;
+  private final ResultCustomInfoService resultCustomInfoService;
+  private final ProvisionResultRepository provisionResultRepository;
 
-    @Operation(summary = "Q&A (출장) 상담 등록", description = "온라인 거래소에서 등록한 Q&A(출장) 상담내용을 상담관리 시스템(IPCC)에 전송합니다.", security = {@SecurityRequirement(name = "bearer-key")})
-    @PostMapping("")
-    public ResponseEntity<JsonResult<Boolean>> RegisterResult(@Parameter(description = "페이지 하단의 ProvisionResultCustomInfoFormRequest를 참조해주세요.") @Valid @RequestBody ProvisionResultCustomInfoFormRequest from) {
-        // 1. 고객키 조회
-
-        // 2. 찾은 고객키 값으로 상담 등록
-
-        // 3. 성공 여부 반환
-
-        return ResponseEntity.ok(data(true));
-    }
+  @Operation(summary = "Q&A (출장) 상담 등록", description = "온라인 거래소에서 등록한 Q&A(출장) 상담내용을 상담관리 시스템(IPCC)에 전송합니다.", security = {
+      @SecurityRequirement(name = "bearer-key")})
+  @PostMapping("")
+  public ResponseEntity<JsonResult<Boolean>> RegisterResult(
+      @Parameter(description = "페이지 하단의 ProvisionResultCustomInfoFormRequest를 참조해주세요.") @Valid @RequestBody ProvisionResultCustomInfoFormRequest request) {
+    provisionResultRepository.saveConsultation(request);
+    return ResponseEntity.ok(data(true));
+  }
 }
