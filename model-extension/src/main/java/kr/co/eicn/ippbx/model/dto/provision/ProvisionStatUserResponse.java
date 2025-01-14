699b7cf0e48f690cce54641b23f36b9a8741b870
RiverStone-0816 <kangrack333@gmail.com>
2025-01-14 18:41:04 +0900

    refactor : IF-CS-006 상담사 실시간 상태 조회 반환값 수정  - 불필요한 엔티티 삭제

--- model-extension/src/main/java/kr/co/eicn/ippbx/model/dto/provision/ProvisionStatUserResponse.java
+++ model-extension/src/main/java/kr/co/eicn/ippbx/model/dto/provision/ProvisionStatUserResponse.java
@@ -5,27 +5,28 @@ import lombok.Data;
 
 @Data
 public class ProvisionStatUserResponse {
-    @Schema(description = "상담원 아이디")
-    private String id;
-    @Schema(description = "상담원 상태명")
-    private String status;
 
-    @Schema(description = "수신 요청호. 인입콜수")
-    private Integer inTotal   = 0;
-    @Schema(description = "수신 응대호. 상담건수")
-    private Integer inSuccess = 0;
-    @Schema(description = "수신 개인비수신. 포기건수")
-    private Integer inCancel  = 0;
+  @Schema(description = "상담원ID")
+  private String userId;
 
-    @Schema(description = "발신 총시도콜")
-    private Integer outTotal   = 0;
-    @Schema(description = "발신 성공호")
-    private Integer outSuccess = 0;
-    @Schema(description = "발신 비수신")
-    private Integer outCancel  = 0;
+  @Schema(description = "상담원명")
+  private String idName;
 
-    @Schema(description = "후처리 건수")
-    private Integer post = 0;
-    @Schema(description = "휴식 건수")
-    private Integer rest = 0;
+  @Schema(description = "상담원상태명")
+  private String statUser;
+
+  @Schema(description = "입입콜수")
+  private Integer inboundTotal;
+
+  @Schema(description = "상담건수")
+  private Integer totalCount;
+
+  @Schema(description = "포기건수")
+  private Integer inboundCancel;
+
+  @Schema(description = "후처리건수")
+  private Integer postProcess;
+
+  @Schema(description = "휴식건수")
+  private Integer rest;
 }
