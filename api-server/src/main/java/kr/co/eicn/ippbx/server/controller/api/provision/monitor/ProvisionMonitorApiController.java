b37d39f96b6a306e01695b78e754ba93423dc654
RiverStone-0816 <kangrack333@gmail.com>
2025-01-15 15:52:24 +0900

    feat : 상담원상태 조회, 컨트롤러 서비스 로직 생성  - 로직 분리  - ProvisionMonitorApiController 호출  - ProvisionMonitorService 로직구현

--- api-server/src/main/java/kr/co/eicn/ippbx/server/controller/api/provision/monitor/ProvisionMonitorApiController.java
+++ api-server/src/main/java/kr/co/eicn/ippbx/server/controller/api/provision/monitor/ProvisionMonitorApiController.java
@@ -1,15 +1,9 @@
 package kr.co.eicn.ippbx.server.controller.api.provision.monitor;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import kr.co.eicn.ippbx.model.dto.provision.ProvisionStatUserResponse;
-import kr.co.eicn.ippbx.server.controller.api.ApiBaseController;
-import kr.co.eicn.ippbx.server.repository.eicn.PersonListRepository;
-import kr.co.eicn.ippbx.server.repository.eicn.QueueMemberTableRepository;
-import kr.co.eicn.ippbx.server.service.StatUserInboundService;
-import kr.co.eicn.ippbx.server.service.StatUserOutboundService;
-import kr.co.eicn.ippbx.util.JsonResult;
+import kr.co.eicn.ippbx.server.service.provision.monitor.ProvisionMonitorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -17,11 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static kr.co.eicn.ippbx.util.JsonResult.data;
 
 /**
  * [API 제공용] 모니터링 API
@@ -31,22 +20,16 @@ import static kr.co.eicn.ippbx.util.JsonResult.data;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "api/provision/monitor", produces = MediaType.APPLICATION_JSON_VALUE)
-public class ProvisionMonitorApiController extends ApiBaseController {
+public class ProvisionMonitorApiController {
 
-    private final PersonListRepository       personListRepository;
-    private final QueueMemberTableRepository queueMemberTableRepository;
-    private final StatUserOutboundService    statUserOutboundService;
-    private final StatUserInboundService     statUserInboundService;
+  private final ProvisionMonitorService provisionMonitorService;
 
-    @Operation(summary = "상담사 실시간 상태 조회", description = "상담관리 시스템(IPCC)의 상담사 현재 상태를 조회합니다.", security = {@SecurityRequirement(name = "bearer-key")})
-    @GetMapping("")
-    public ResponseEntity<JsonResult<List<ProvisionStatUserResponse>>> list() {
-        final List<ProvisionStatUserResponse> list = new ArrayList<>();
-
-        // 상담원 상태 정보 조회 로직
-
-
-        // 반환
-        return ResponseEntity.ok(data(list));
-    }
+  /**
+   * 상담원 상태 정보를 조회
+   */
+  @GetMapping("")
+  public ResponseEntity<List<ProvisionStatUserResponse>> getAgentCallStatus() {
+    var result = provisionMonitorService.getAgentCallStatus();
+    return ResponseEntity.ok(result);
+  }
 }
---
+++ api-server/src/main/java/kr/co/eicn/ippbx/server/service/provision/monitor/ProvisionMonitorService.java
@@ -0,0 +1,61 @@
+package kr.co.eicn.ippbx.server.service.provision.monitor;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import kr.co.eicn.ippbx.model.dto.customdb.PersonLastStatusInfoResponse;
+import kr.co.eicn.ippbx.model.dto.provision.ProvisionStatUserResponse;
+import kr.co.eicn.ippbx.model.search.StatUserSearchRequest;
+import kr.co.eicn.ippbx.server.controller.api.v1.admin.stat.user.StatUserApiController;
+import kr.co.eicn.ippbx.server.service.MemberStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProvisionMonitorService {
+
+  private final StatUserApiController statUserApiController;
+  private final MemberStatusService memberStatusService;
+
+  /**
+   * 상담원 상태 정보를 조회 및 매핑
+   */
+  public List<ProvisionStatUserResponse> getAgentCallStatus() {
+    // StatUser 데이터를 가져오기
+    var statUserResponse = statUserApiController.getTodayAgentActivitySummary(new StatUserSearchRequest());
+    var responseData = Objects.requireNonNull(statUserResponse.getBody()).getData();
+
+    // 상담원 상태 데이터를 가져오기
+    List<PersonLastStatusInfoResponse> filterPersonStatusInfo = memberStatusService.getAllPersonStatusInfo();
+
+    // 데이터를 매핑하여 반환
+    return responseData.stream().flatMap(stat ->
+        stat.getUserStatList().stream().map(userStat -> {
+          ProvisionStatUserResponse provisionResponse = new ProvisionStatUserResponse();
+          provisionResponse.setUserId(userStat.getUserId());
+          provisionResponse.setIdName(userStat.getIdName());
+          provisionResponse.setStatUser(getNextStatus(filterPersonStatusInfo, userStat.getUserId()));
+          provisionResponse.setInboundTotal(userStat.getInboundStat().getTotal());
+          provisionResponse.setInboundCancel(userStat.getInboundStat().getCancel());
+          provisionResponse.setTotalCount(userStat.getTotalCnt());
+          Map<Integer, Long> statusCountMap = userStat.getMemberStatusStat().getStatusCountMap();
+          provisionResponse.setPostProcess(Math.toIntExact(statusCountMap.getOrDefault(2, 0L)));
+          provisionResponse.setRest(Math.toIntExact(statusCountMap.getOrDefault(3, 0L)));
+          return provisionResponse;
+        })
+    ).collect(Collectors.toList());
+  }
+
+  /**
+   * 상담원의 다음 상태를 조회
+   */
+  private Integer getNextStatus(List<PersonLastStatusInfoResponse> filterPersonStatusInfo, String userId) {
+    return filterPersonStatusInfo.stream()
+        .filter(info -> userId.equals(info.getPhonename()))
+        .map(PersonLastStatusInfoResponse::getNextStatus)
+        .findFirst()
+        .orElse(null);
+  }
+}
