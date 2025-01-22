5ddeb01d2e210f3fd4de8b84d71ac83643b9d3da
RiverStone <kangrack333@gmail.com>
2025-01-22 19:04:06 +0900

    Delete vue-messenger directory

--- vue-messenger/src/utillities/compositions/useDebounce.js
+++
@@ -1,9 +0,0 @@
-import {ref} from "vue"
-
-export default () => {
-    const timeout = ref(null)
-    return (func, wait = 500) => {
-        clearTimeout(timeout.value)
-        timeout.value = setTimeout(func, wait)
-    }
-}
--- vue-messenger/src/utillities/compositions/useWindowEvent.js
+++
@@ -1,6 +0,0 @@
-import {onMounted, onUnmounted} from "vue";
-
-export default function (eventType, handler) {
-    onMounted(() => window.addEventListener(eventType, handler))
-    onUnmounted(() => window.removeEventListener(eventType, handler))
-}
