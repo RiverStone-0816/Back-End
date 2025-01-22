65a4a4050ef56b7c2073c762fc3b6256ab08bcec
RiverStone-0816 <kangrack333@gmail.com>
2025-01-22 17:12:21 +0900

    refactor : 상담내역관리 SEQ를 통한 조회 서비스 구성

--- api-server/src/main/java/kr/co/eicn/ippbx/server/service/ResultCustomInfoService.java
+++ api-server/src/main/java/kr/co/eicn/ippbx/server/service/ResultCustomInfoService.java
@@ -1,5 +1,8 @@
 package kr.co.eicn.ippbx.server.service;
 
+import java.util.HashMap;
+import java.util.Map;
+import kr.co.eicn.ippbx.model.dto.customdb.ResultCustomInfoFromResponse;
 import kr.co.eicn.ippbx.server.repository.customdb.ResultCustomInfoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,25 +11,26 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 public class ResultCustomInfoService extends ApiBaseService implements ApplicationContextAware {
-    protected final Logger logger = LoggerFactory.getLogger(ResultCustomInfoService.class);
-    private final Map<String, ResultCustomInfoRepository> repositories = new HashMap<>();
-    private ApplicationContext applicationContext;
+  protected final Logger logger = LoggerFactory.getLogger(ResultCustomInfoService.class);
+  private final Map<String, ResultCustomInfoRepository> repositories = new HashMap<>();
+  private ApplicationContext applicationContext;
 
-    public ResultCustomInfoRepository getRepository(){
-        return repositories.computeIfAbsent(g.getUser().getCompanyId(), companyId -> {
-            final ResultCustomInfoRepository repository = new ResultCustomInfoRepository(companyId);
-            applicationContext.getAutowireCapableBeanFactory().autowireBean(repository);
-            return repository;
-        });
-    }
+  public ResultCustomInfoRepository getRepository() {
+    return repositories.computeIfAbsent(g.getUser().getCompanyId(), companyId -> {
+      final ResultCustomInfoRepository repository = new ResultCustomInfoRepository(companyId);
+      applicationContext.getAutowireCapableBeanFactory().autowireBean(repository);
+      return repository;
+    });
+  }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    this.applicationContext = applicationContext;
+  }
+
+  public ResultCustomInfoFromResponse getCombinedResultCustomInfoBySeq(Integer seq) {
+    return getRepository().getCombinedResultCustomInfoBySeq(seq);
+  }
 }
