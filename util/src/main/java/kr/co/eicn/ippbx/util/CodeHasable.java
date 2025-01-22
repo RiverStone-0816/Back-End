3b9e24b578fdd38f21ddb17302a176404c260322
RiverStone <kangrack333@gmail.com>
2025-01-22 19:04:19 +0900

    Delete util directory

--- util/src/main/java/kr/co/eicn/ippbx/util/CodeHasable.java
+++
@@ -1,5 +0,0 @@
-package kr.co.eicn.ippbx.util;
-
-public interface CodeHasable<T> {
-    T getCode();
-}
--- util/src/main/java/kr/co/eicn/ippbx/util/SortField.java
+++
@@ -1,5 +0,0 @@
-package kr.co.eicn.ippbx.util;
-
-public interface SortField {
-	String field();
-}
