9370cfb6f384011db930cc9434cb88cd70afb323
RiverStone <kangrack333@gmail.com>
2025-01-22 19:03:45 +0900

    Delete api-server directory

--- api-server/src/main/java/kr/co/eicn/ippbx/server/config/MybatisPersistenceConfig.java
+++
@@ -1,9 +0,0 @@
-package kr.co.eicn.ippbx.server.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan({Constants.REPOSITORY_PACKAGE})
-public class MybatisPersistenceConfig {
-}
--- api-server/src/main/java/kr/co/eicn/ippbx/server/repository/eicn/TestMapper.java
+++
@@ -1,7 +0,0 @@
-package kr.co.eicn.ippbx.server.repository.eicn;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TestMapper {
-}
